9759c3eedf6758f85f72c25f1b6fe8056afab8e4
Hassan <hassan.subhani@codeteck.com>
2024-08-12 13:52:00 +0500

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [ 
+          Container(
+            height: 300,
+            width: 200,
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
+      ),
+    );
+  }
+}
